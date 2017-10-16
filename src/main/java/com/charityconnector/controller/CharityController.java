b4ba51b4e4ce6ade65f6c76d20c1b667247b067c
Requiel20 <luca.battistelli@studenti.unicam.it>
2017-10-16 21:54:19 +0100

    removed redundant comments, clarified method names

--- src/main/java/com/charityconnector/controller/CharityController.java
+++ src/main/java/com/charityconnector/controller/CharityController.java
@@ -8,87 +8,47 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import java.util.Map;
 
-/**
- * because we combine two different types of apis, so here only use Controller annotation
- */
 @Controller
 public class CharityController {
 
     @Resource
     private CharityService charityService;
 
-    /**
-     * Get charities by name.
-     * Charities may have same name.
-     *
-     * @param name
-     * @return
-     */
     @RequestMapping(path = "/charities/{name}", method = RequestMethod.GET)
     @ResponseBody
     Charity[] getCharitiesByName(@PathVariable("name") String name) {
         return charityService.findByName(name);
     }
 
-    /**
-     * Charity has unique Id so we find one charity by it's id.
-     *
-     * @param id
-     * @return
-     */
     @RequestMapping(path = "/charity/{id}", method = RequestMethod.GET)
     @ResponseBody
     Charity getCharityById(@PathVariable("id") Long id) {
         return charityService.findById(id);
     }
 
-    /**
-     * Add a new charity.
-     *
-     * @param charity
-     * @return
-     */
     @RequestMapping(path = "/charity", method = RequestMethod.POST)
     @ResponseBody
-    public Charity add(@RequestBody Charity charity) {
+    public Charity addCharity(@RequestBody Charity charity) {
         Charity res = charityService.addCharity(charity);
         return res;
     }
 
-    /**
-     * Delete a charity by description.
-     *
-     * @param id
-     * @return
-     */
     @RequestMapping(path = "/charity/{id}", method = RequestMethod.DELETE)
     @ResponseBody
-    public void removeCharityByDescription(@PathVariable("id") Long id ) {
+    public void deleteCharityById(@PathVariable("id") Long id) {
         charityService.deleteById(id);
     }
 
-    /**
-     * Update a particular Charity.
-     *
-     * @param charity
-     * @return
-     */
     @RequestMapping(path = "/charity", method = RequestMethod.PATCH)
     @ResponseBody
-    public void update(@RequestBody Charity charity) {
+    public void updateCharity(@RequestBody Charity charity) {
         charityService.update(charity);
     }
 
-    /**
-     * Provide a api to retrieve charity page.
-     *
-     * @return
-     */
+
     @RequestMapping("/charityPage/{id}")
-    public String charityPage(Map<String, Object> model, @PathVariable("id") Long id) {
+    public String getcharityPage(Map<String, Object> model, @PathVariable("id") Long id) {
 	    model.put("charity", charityService.findById(id));
 	    return "charityPage";
     }
-
-
 }
