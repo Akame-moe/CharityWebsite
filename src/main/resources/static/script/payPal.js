2d9c47e010af1ab23629c6361ea39083ca765243
Requiel20 <luca.battistelli@studenti.unicam.it>
2017-10-24 18:27:59 +0100

    switched to frontend-only solution, working example

---
+++ src/main/resources/static/script/payPal.js
@@ -0,0 +1,31 @@
+paypal.Button.render({
+
+    env: 'sandbox', // Or 'sandbox'
+
+    client: {
+        sandbox: 'AZ4rzAJMd3YLKr5-Av9DuqvF0LdsnIN65aRFQpbNF78-jATGOCKDKHQ9Idk0yIXZuX2E7kJ-xqkp3yI1',
+        production: 'xxxxxxxxx'
+    },
+
+    commit: true, // Show a 'Pay Now' button
+
+    payment: function (data, actions) {
+        return actions.payment.create({
+            payment: {
+                transactions: [
+                    {
+                        amount: {total: $('#inputDonationAmount').val(), currency: 'GBP'}
+                    }
+                ]
+            }
+        });
+    },
+
+    onAuthorize: function (data, actions) {
+        return actions.payment.execute().then(function (payment) {
+            $('#donateModal').modal('toggle');
+            $('#donateSuccessModal').modal('toggle');
+        });
+    }
+
+}, '#paypal-button');
