8161b88d513ebf760b27dbeb2bbca7157552fbbc
qz2316 <qian.zhang17@imperial.ac.uk>
2017-11-14 08:18:46 +0000

    add select list in html

--- src/main/java/com/charityconnector/controller/SearchController.java
+++ src/main/java/com/charityconnector/controller/SearchController.java
@@ -26,7 +26,8 @@ public class SearchController {
     }
 
     @RequestMapping("/results")
-    public String getResultsPage(Map<String, Object> model, @RequestParam(defaultValue = "") String name, @RequestParam(defaultValue = "0") int pageNumber, @RequestParam(defaultValue = "10") int pageSize) {
+    public String getResultsPage(Map<String, Object> model, @RequestParam(defaultValue = "") String name,
+                                 @RequestParam(defaultValue = "0") int pageNumber, @RequestParam(defaultValue = "10") int pageSize) {
         if (name == null || name.trim().equals(""))
             return "resultsPage";
         if (pageSize != 10 && pageSize != 25 && pageSize != 50)
@@ -46,4 +47,31 @@ public class SearchController {
 
         return "resultsPage";
     }
+
+
+
+    /*@RequestMapping("/results")
+    public String getResultsPage(Map<String, Object> model, @RequestParam(defaultValue = "") String keyword,
+                                 @RequestParam(defaultValue = "0") int pageNumber, @RequestParam(defaultValue = "10") int pageSize,
+                                 @RequestParam(defaultValue = "name") String category) {
+        if (keyword == null || keyword.trim().equals(""))
+            return "resultsPage";
+        if (pageSize != 10 && pageSize != 25 && pageSize != 50)
+            pageSize = 10;
+        PageRequest pageRequest = new PageRequest(pageNumber, pageSize, Sort.Direction.DESC, "name");
+        if
+        Page<Charity> page = charityService.findByNameLike("%" + keyword + "%", pageRequest);
+        List<Charity> charities = page.getContent();
+        if (charities.size() != 0) {
+            model.put("charities", charities);
+            model.put("numberOfPages", page.getTotalPages());
+            model.put("numberOfResults", page.getTotalElements());
+            model.put("pageNumber", page.getNumber());
+            model.put("searchedName", keyword);
+            model.put("pageSize", pageSize);
+            model.put("thisPageSize", charities.size());
+        }
+
+        return "resultsPage";
+    }*/
 }
