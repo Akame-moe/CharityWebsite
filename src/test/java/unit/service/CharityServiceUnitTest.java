0dd32898b98754f7d26ce4600a6b28515d69c204
JunbinWang <398712463@qq.com>
2017-12-25 22:32:45 +0000

    fixed login redirect bug

--- src/test/java/unit/service/CharityServiceUnitTest.java
+++ src/test/java/unit/service/CharityServiceUnitTest.java
@@ -91,9 +91,9 @@ public class CharityServiceUnitTest {
 
     @Test
     public void selectiveUpdatingCharity() {
-        Charity oldCharity = new Charity(1L, "name", "description", "123", "email", "paypal", null, null, null,"verifyCode", 1, 1L);
+        Charity oldCharity = new Charity(1L, "name", "description", "123", "email", "paypal", null, null, null,"verifyCode", 1, 1L,null);
 
-        Charity newCharity = new Charity(1L, null, "adifferentDescription", null, "email2", "paypal", null, null, null, null, 0, 2L);
+        Charity newCharity = new Charity(1L, null, "adifferentDescription", null, "email2", "paypal", null, null, null, null, 0, 2L,null);
 
         when(mockRepo.findOne(1L)).thenReturn(oldCharity);
 
